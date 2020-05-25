b334cc133703ee0d0ea927b0e8bb57d869f06b03
dlgeraghty <dvd.loge@gmail.com>
2020-05-25 15:57:56 +0200

    ADT license 2

--- ED/34.cpp
+++ ED/34.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string>
+#include <list>
 #include "34.h"
 
 using namespace std;
@@ -29,10 +31,23 @@ bool resuelveCaso(){
 				int x = d.consultar(s); //para que produzca la excepcion si es necesario
 				printf("Puntos de %s: %d\n", s.c_str(), x);
 			}
+			else if(s == "recuperar"){
+				cin >> s >> n;
+				d.recuperar(s, n);
+			}
 			else if(s == "cuantos_con_puntos"){
 				cin >> n;
 				int x = d.cuantos_con_puntos(n);
 				printf("Con %d puntos hay %d\n", n, x);
+			}
+			else if(s == "lista_por_puntos"){
+				cin >> n;
+				list<string> v = d.lista_por_puntos(n);
+				printf("Tienen %d puntos: ", n);
+				v.reverse();
+				for(string i: v)
+					printf("%s ", i.c_str());
+				printf("\n");
 			}
 		}catch(domain_error e){
 			cout << "ERROR: " << e.what() << '\n';
--- ED/34.h
+++ ED/34.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <unordered_map>
 #include <string>
+#include <list>
 
 using namespace std;
 
@@ -8,33 +9,62 @@ using namespace std;
 #define valor second
 
 class carnet_puntos{
-	unordered_map<string, int> carnet;
-	unordered_map<int, int> cuantos;
+	unordered_map<string, pair<int, list<string>::iterator>> carnet;
+	unordered_map<int, list<string>> cuantos;
 
 public:
 	void nuevo(string dni){
-		auto res = carnet.insert({dni, 15});
-		if(!res.second)
+		auto it = carnet.find(dni);
+		if(it != carnet.end())
 			throw domain_error("Conductor duplicado");
-		else
-			cuantos[15]++;
+		else{
+			carnet[dni] = {15, {}};
+			auto & ip = cuantos[15];
+			carnet[dni].second = ip.insert(ip.end(), dni);
+		}
 	}
 	void quitar(string dni, int puntos){
-		if(carnet.find(dni) != carnet.end()){
-			cuantos[carnet[dni]]--;
-			carnet[dni] -= puntos;
-			if(carnet[dni] < 0) carnet[dni] = 0;
-			cuantos[carnet[dni]]++;
+		auto it = carnet.find(dni);
+		if(it != carnet.end()){
+			if(it->valor.first != 0){
+				auto &p = it->valor.first;
+				auto &ip = cuantos[p];
+				ip.erase(it->valor.second);
+				p -= puntos;
+				if(p < 0) p = 0;
+				it->valor.second = cuantos[p].insert(cuantos[p].end(), dni);
+			}
 		}else
 			throw domain_error("Conductor inexistente");
 	}
-	int consultar(string dni){
-		if(carnet.find(dni) != carnet.end()){
-			return carnet[dni];
+	void recuperar(string dni, int puntos){
+		auto it = carnet.find(dni);
+		if(it != carnet.end()){
+			if(it->valor.first != 15){
+				auto &p = it->valor.first;
+				auto &ip = cuantos[p];
+				ip.erase(it->valor.second);
+				p += puntos;
+				if(p > 15) p = 15;
+				it->valor.second = cuantos[p].insert(cuantos[p].end(), dni);
+			}
 		}else
 			throw domain_error("Conductor inexistente");
 	}
-	int cuantos_con_puntos(int puntos){
+	int consultar(string dni) {
+		if(carnet.find(dni) != carnet.end()){
+			return carnet[dni].first;
+		}else
+			throw domain_error("Conductor inexistente");
+	}
+	int cuantos_con_puntos(int puntos) {
+		if(puntos >= 0 && puntos <= 15){
+			return cuantos[puntos].size();
+		}else
+			throw domain_error("Puntos no validos");
+	}
+
+	list<string> lista_por_puntos(int puntos) {
 		if(puntos >= 0 && puntos <= 15){
 			return cuantos[puntos];
 		}else
